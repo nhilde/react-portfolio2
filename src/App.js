eba9ca5e49d5d5691c02c1f400ae660268af9db6
Noah Hildebrand <nhilde21@gmail.com>
2022-05-24 15:25:29 -0400

    routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
 import Homepage from './components/homepage';
-import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { HashRouter as Router, Routes, Route} from 'react-router-dom';
 import Work from './components/work';
 import AboutMe from './components/aboutMe'
 import Contact from './components/contact';
